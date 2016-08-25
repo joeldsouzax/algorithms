6df694be9cfbc9e49b7b04c65584e455baee77ff
Joel DSouza <joedso1@gmail.com>
2016-08-25 20:25:49 +0530

    added push in linkedlist in C

--- C/LinkedList.c
+++ C/LinkedList.c
@@ -10,12 +10,17 @@ struct node * next; /* pointer variable to store memory location of next node*/
 };
 
 struct node * create(int); /* function declaration to create a node */
+void push(struct node **, int); /* function to push a node on the head of the linked list*/
 
 /* main method as a client that will create and use the list*/
 
 int main(){
 struct node * head = create(10); /* create head node and pass data 10*/
-printf("%d",head->data);
+printf("%d\n",head->data);
+push(&head, 12);
+printf("%d\n",head->data);
+push(&head, 17);
+printf("%d\n",head->data);
 return 0;
 }
 
@@ -27,4 +32,11 @@ struct node * create(int x){
     head->next = NULL; /* pointing to a null location indicating the end*/
     
     return head;
+}
+
+void push(struct node ** headref, int x){
+ struct node * new = malloc(sizeof(struct node)); /* creating a memory for the node*/
+ new->data = x;
+ new->next= *headref; /* pass the value of the next in the memory allocation of head*/
+ *headref = new; 
 }
