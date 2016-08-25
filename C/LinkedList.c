dc2a5aaa2a9943ae6640aee71f2285a9253e9388
Joel DSouza <joedso1@gmail.com>
2016-08-25 21:44:15 +0530

    Added append to the linked list in C

--- C/LinkedList.c
+++ C/LinkedList.c
@@ -11,7 +11,8 @@ struct node * next; /* pointer variable to store memory location of next node*/
 
 struct node * create(int); /* function declaration to create a node */
 void push(struct node **, int); /* function to push a node on the head of the linked list*/
-int length(struct node *); /* count the number of nodes in a linked list*/
+int len(struct node *); /* count the number of nodes in a linked list*/
+void append(struct node **, int); /* appending the node at the end of the list */
 
 /* main method as a client that will create and use the list*/
 
@@ -22,7 +23,9 @@ push(&head, 12);
 printf("%d\n",head->data);
 push(&head, 17);
 printf("%d\n",head->data);
-printf("%d\n",length(head));
+printf("%d the length \n",len(head));
+append(&head,19);
+printf("%d the length \n",len(head));
 return 0;
 }
 
@@ -44,11 +47,32 @@ void push(struct node ** headref, int x){
 }
 
 /*share the head pointer*/
-int count(struct node * head){
+int len(struct node * head){
 struct node * current = head; /*copy the head pointer to another one to itirate over and get the count*/
 int count = 0;
 while(current != NULL){
 current = current->next;
 count++;
 }
+return count;
 }
+
+void append(struct node ** headref, int x){
+        struct node * new = malloc(sizeof(struct node));
+        struct node * current = *headref;
+
+        new->data = x;
+        new->next = NULL;
+
+        if(current==NULL){
+        *headref = new;
+        }else{
+        while(current->next !=NULL){
+        current = current->next;}
+
+        current->next=new;
+
+        }
+}
+
+
